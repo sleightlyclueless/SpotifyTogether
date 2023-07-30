5da9210e4c6a7a6908826dd1d310a7aeff993383
Sebastian <seppizill@gmx.de>
2023-07-30 21:12:27 +0200

    Last minute fix JoinEventByQr jsx component

--- frontend/src/pages/JoinEventByQr.tsx
+++ frontend/src/pages/JoinEventByQr.tsx
@@ -2,7 +2,7 @@ import { useJoinEvent } from "../hooks";
 import { PageContainer, StyledTextL } from "../styles";
 
 // This page just functions to access the joinEvent (qr) hook with event in url param
-export const JoinEventByQr = async (): Promise<JSX.Element | null> => {
+export const JoinEventByQr = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const eventID = urlParams.get("event") || null;
   const { joinEvent } = useJoinEvent();
